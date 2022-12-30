d4ae67ed2c498f2d61abb82df1ec72fa98ba5e5a
Alejandro Iruleguy <ale.iruleguy@gmail.com>
2022-12-30 12:57:00 -0300

    Initial commit.

---
+++ src/handlers/index.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const totp = require("totp-generator");
+const axios = require('axios');
+const axiosRetry = require('axios-retry');
+
+const TIMEOUT = 1000
+const RETRY_DELAY = 300
+const RETRY_COUNT = 3
+
+exports.handler = async(event) => {
+    console.debug(`Event received is: ${JSON.stringify(event)}`);
+    
+    const token = totp("JBSWY3DPEHPK3PXPXX");
+    const url = "https://hooks.slack.com/services/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";
+
+    axiosRetry(axios, {
+        retries: RETRY_COUNT,
+        shouldResetTimeout: true,
+        retryDelay: (retryCount) => {
+            return RETRY_DELAY
+        },
+        retryCondition: (error) => {
+            console.error(error)
+            return isServerError(error) || isTimeoutError(error)
+        },
+    });
+    
+    await axios.post(url, {text: `Your token is: ${token}`}, { timeout: TIMEOUT, headers: {'Content-Type': 'application/json'} })
+        .then(data => {
+            //metrics.counter('middleware.post.response.ok')
+            console.debug(`Response from Slack: ${data}`)
+            return data;
+        })
+        .catch(error => {
+            if (error.response) {
+                console.error(`[TOTP] Error sending message to Slack - status: ${error.response.status} - response: ${error.response.data}`)
+                //metrics.counter(`middleware.post.response.${error.response.status}`)
+            } else if (isTimeoutError(error)) {
+                console.error(`[TOTP] Error sending message to Slack - TIMEOUT`)
+                //metrics.counter(`middleware.post.response.timeout`)
+            }
+            throw error;
+        });
+};
+
+function isTimeoutError(error) {
+    return error.code === 'ECONNABORTED'
+}
+
+function isServerError(error) {
+    return error.response && error.response.status >= 500 && error.response.status < 600
+}
